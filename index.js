fdb8b08a53f7775bb5c324226b0b90b0eba1b987
Erik Gomes Siqueira <erikgomessiqueira.estudante@gmail.com>
2022-10-22 09:11:50 -0300

    bug_fix: Mudando URL do Banco de dados

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ app.get('/', (req, res) =>{
     res.status(200).json({message: "Bem vindo"})
 })
 
-// mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.29fyyuk.mongodb.net/?retryWrites=true&w=majority`) 
-    mongoose.connect(`mongodb://${dbUser}:${dbPassword}@localhost:27017/Ra?authSource=${dbPermission}`)
+// mongoose.connect(`mongodb://${dbUser}:${dbPassword}@localhost:27017/Ra?authSource=${dbPermission}`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.29fyyuk.mongodb.net/?retryWrites=true&w=majority`) 
     .then(()=>{
         console.log("Conectado ao Banco")
         app.listen(port, () => console.log(`A API esta rodando na porta ${port}!`))
